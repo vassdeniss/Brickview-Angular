21178170a07d008ec14831845fdef203bf26dc95
Denis Vasilev <vassdeniss@gmail.com>
2023-06-23 15:34:25 +0300

    Server: Added database connection

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const connectDb = require('./configs/database');
+
+connectDb()
+  .then(() => console.log('Connected to database'))
+  .catch((err) => console.log(`DB error: ${err}`));
+
 app.get('/', (req, res) => {
   res.send('Works!');
 });
