36459c4378593b77bd5bb5cd9a1dbe378f576330
Denis Vasilev <vassdeniss@gmail.com>
2023-06-26 19:05:01 +0300

    Server: Basic user model

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Username is required!'],
+    minLength: [4, 'Username must be at least 4 characters long!'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required!'],
+    minLength: [8, 'Password must be at least 8 characters long!'],
+  },
+});
+
+userSchema.virtual('repeatPassword').set(function (value) {
+  if (!value) {
+    this.invalidate('repeatPassword', 'Repeat password is required!');
+  }
+
+  if (value !== this.password) {
+    this.invalidate('repeatPassword', 'Password mismatch!');
+  }
+});
+
+userSchema.pre('save', async function () {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
