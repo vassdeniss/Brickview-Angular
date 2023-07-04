d0e3f8eda0de0cfbb9bc2737c4ddca7b5b7ae8f0
Denis Vasilev <vassdeniss@gmail.com>
2023-07-04 23:02:08 +0300

    Client: Added environment variables

--- client/src/app/services/set.service.ts
+++ client/src/app/services/set.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Set } from '../types/setType';
+
 import { Observable } from 'rxjs';
+
+import { Set } from '../types/setType';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class SetService {
   constructor(private http: HttpClient) {}
 
   getSet(setId: string): Observable<Set> {
-    return this.http.get<Set>(`http://localhost:3000/sets/${setId}`);
+    return this.http.get<Set>(`${environment.apiUrl}/sets/${setId}`);
   }
 }
