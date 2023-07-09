a0165f9a8f3f38eea7198b42720944fc4ef6039e
Denis Vasilev <vassdeniss@gmail.com>
2023-07-09 15:11:06 +0300

    Server: Improved auth middleware for token validation

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,19 +1,41 @@
 const jwt = require('../lib/jwt');
+const userService = require('../services/userService');
 
-exports.mustBeAuth = (req, res, next) => {
-  const token = req.header('X-Autherization');
+exports.mustBeAuth = async (req, res, next) => {
+  const token = req.header('X-Authorization');
 
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-      next();
-    } catch (err) {
-      res.status(400).json({
-        meesage: 'You are not authorized!',
-      });
+  try {
+    if (!token) {
+      throw new Error('You are not authorized!');
     }
-  } else {
+
+    try {
+      await jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      if (err.name !== 'TokenExpiredError') {
+        throw err;
+      }
+
+      const refreshToken = req.header('X-Refresh');
+      if (!refreshToken) {
+        throw new Error('Invalid refresh token!');
+      }
+
+      const user = await userService.validateRefreshToken(refreshToken);
+
+      const newTokens = await userService.generateToken(user);
+      res.set('X-Refresh', newTokens.refreshToken);
+      res.set('X-Authorization', newTokens.accessToken);
+      res.set('access-control-expose-headers', [
+        'X-Authorization',
+        'X-Refresh',
+      ]);
+    }
+
     next();
+  } catch (err) {
+    res.status(401).json({
+      message: err.message || 'You are not authorized!',
+    });
   }
 };
