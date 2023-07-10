46ca8aeaff04780953d924f5a5f8a79a632c9926
Denis Vasilev <vassdeniss@gmail.com>
2023-07-10 20:38:58 +0300

    Client: Tested the popup component

--- client/src/app/shared/popup/popup.component.spec.ts
+++ client/src/app/shared/popup/popup.component.spec.ts
@@ -3,19 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PopupComponent } from './popup.component';
 
 describe('PopupComponent', () => {
-  let component: PopupComponent;
-  let fixture: ComponentFixture<PopupComponent>;
-
-  beforeEach(() => {
+  it('raises the selected event when clicked', () => {
+    // Arrange: configure the component for testing
     TestBed.configureTestingModule({
-      declarations: [PopupComponent]
+      declarations: [PopupComponent],
     });
-    fixture = TestBed.createComponent(PopupComponent);
-    component = fixture.componentInstance;
+    const fixture: ComponentFixture<PopupComponent> =
+      TestBed.createComponent(PopupComponent);
+    const component = fixture.componentInstance;
     fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    // Arrange: spy on the event emitter
+    spyOn(component.closed, 'emit');
+
+    // Act: call the component method
+    component.closePopup();
+
+    // Assert: that the event emitter has been called
+    expect(component.closed.emit).toHaveBeenCalled();
   });
 });
