31a873bb1005411b1efc071c42316bdce55ff085
Denis Vasilev <vassdeniss@gmail.com>
2023-07-13 01:15:11 +0300

    Client: Exported page spinner

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PageSpinnerComponent } from './page-spinner/page-spinner.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [NavComponent, PagenotfoundComponent],
-  imports: [CommonModule],
-  exports: [NavComponent],
+  declarations: [NavComponent, PagenotfoundComponent, PageSpinnerComponent],
+  imports: [CommonModule, SharedModule],
+  exports: [NavComponent, PageSpinnerComponent],
 })
 export class CoreModule {}
