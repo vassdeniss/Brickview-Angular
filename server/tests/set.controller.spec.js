99c80844fe9ab7dde24720629e63cee7d2ddc800
Denis Vasilev <vassdeniss@gmail.com>
2023-07-18 18:19:21 +0300

    Server: Added set router for adding / getting sets

--- server/tests/set.controller.spec.js
+++ server/tests/set.controller.spec.js
@@ -1,43 +1,79 @@
-const express = require('express');
 const request = require('supertest');
 const sinon = require('sinon');
-const { expect } = require('chai');
+const sinonChai = require('sinon-chai');
+const chai = require('chai');
+const { expect } = chai;
+
+chai.use(sinonChai);
 
 const setService = require('../services/setService');
-const router = require('../controllers/setController');
-const { mockedSetData } = require('./mockedData');
+const authMiddleware = require('../middlewares/auth');
 
-const app = express();
-app.use('/', router);
+let app;
 
 describe('Set controller routes', function () {
+  beforeEach(() => {
+    sinon.stub(authMiddleware, 'mustBeAuth').callsFake((req, res, next) => {
+      next();
+    });
+
+    app = require('../server');
+  });
+
   afterEach(() => {
     sinon.restore();
   });
 
-  describe('GET /:setId', () => {
-    it('should return set data and status 200 when a valid setId is provided', async () => {
-      const setId = '12345';
+  describe('GET /logged-user-collection', () => {
+    it('should return 200 with the user collection', async () => {
+      sinon
+        .stub(setService, 'getLoggedInUserCollection')
+        .resolves({ mockData: 'collection' });
 
-      sinon.stub(setService, 'getWithMinifigs').resolves(mockedSetData);
-
-      const response = await request(app).get(`/${setId}`);
+      const response = await request(app)
+        .get('/sets/logged-user-collection')
+        .set('X-Refresh', 'mockRefreshToken');
 
       expect(response.status).to.equal(200);
-      expect(response.body).to.deep.equal(mockedSetData);
+      expect(response.body).to.deep.equal({ mockData: 'collection' });
     });
 
-    it('should return status 404 and "Set not found!" message when setService throws an error', async () => {
-      const setId = 'invalidId';
-
+    it('should return 404 if an error occurs', async () => {
       sinon
-        .stub(setService, 'getWithMinifigs')
-        .rejects(new Error('Set not found!'));
+        .stub(setService, 'getLoggedInUserCollection')
+        .throws(new Error('mockError'));
 
-      const response = await request(app).get(`/${setId}`);
+      const response = await request(app)
+        .get('/sets/logged-user-collection')
+        .set('X-Refresh', 'mockRefreshToken');
 
       expect(response.status).to.equal(404);
-      expect(response.text).to.equal('Set not found!');
+      expect(response.body.message).to.equal('mockError');
+    });
+  });
+
+  describe('POST /add-set', () => {
+    it('should return 204 if set is added successfully', async () => {
+      sinon.stub(setService, 'addSet');
+
+      const response = await request(app)
+        .post('/sets/add-set')
+        .set('X-Refresh', 'mockRefreshToken')
+        .send({ setId: 'mockSetId' });
+
+      expect(response.status).to.equal(204);
+    });
+
+    it('should return 404 if the set is not found', async () => {
+      sinon.stub(setService, 'addSet').throws(new Error('Set not found!'));
+
+      const response = await request(app)
+        .post('/sets/add-set')
+        .set('X-Refresh', 'mockRefreshToken')
+        .send({ setId: 'mockSetId' });
+
+      expect(response.status).to.equal(404);
+      expect(response.body.message).to.equal('Set not found!');
     });
   });
 });
