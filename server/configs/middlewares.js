c77b7a9976132272bbf84843b9df15cf16575876
Denis Vasilev <vassdeniss@gmail.com>
2023-07-22 00:59:42 +0300

    Server: Changed payload allowed size

--- server/configs/middlewares.js
+++ server/configs/middlewares.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 
 function addMiddlewares(app) {
   app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json({ limit: '500mb' }));
+  app.use(express.urlencoded({ extended: false, limit: '500mb' }));
 }
 
 module.exports = addMiddlewares;
