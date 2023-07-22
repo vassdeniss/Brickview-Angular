b1d02cceb9ff77a3106cc1590022b0354479acd3
Denis Vasilev <vassdeniss@gmail.com>
2023-07-22 11:11:35 +0300

    Fixed CI/CD pipeline

--- server/configs/middlewares.js
+++ server/configs/middlewares.js
@@ -2,9 +2,16 @@ const express = require('express');
 const cors = require('cors');
 
 function addMiddlewares(app) {
-  app.use(cors());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:4200',
+        'https://vassdeniss.github.io/LegoView-Angular',
+      ],
+    })
+  );
+  app.use(express.json({ limit: '500mb' }));
+  app.use(express.urlencoded({ extended: false, limit: '500mb' }));
 }
 
 module.exports = addMiddlewares;
