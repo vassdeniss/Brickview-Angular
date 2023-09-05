d63f3c767638dd711c57b6e01f461f4971857319
Denis Vasilev <vassdeniss@gmail.com>
2023-09-06 01:00:05 +0300

    Client: Moved home to all-reviews component

---
+++ client/src/app/review/all-reviews/all-reviews.resolver.ts
@@ -0,0 +1,7 @@
+import { inject } from '@angular/core';
+import { ResolveFn } from '@angular/router';
+import { SetService } from 'src/app/services/set.service';
+import { Set } from 'src/app/types/setType';
+
+export const allReviewsResolver: ResolveFn<Set[]> = (route, _) =>
+  inject(SetService).getAll(route.queryParams['setNumber']);
--- client/src/app/review/review-routing.module.ts
+++ client/src/app/review/review-routing.module.ts
@@ -6,8 +6,17 @@ import { DetailReviewComponent } from './detail-review/detail-review.component';
 import { detailReviewResolver } from './detail-review/detail-review.resolver';
 import { EditReviewComponent } from './edit-review/edit-review.component';
 import { editReviewResolver } from './edit-review/edit-review.resolver';
+import { AllReviewsComponent } from './all-reviews/all-reviews.component';
+import { allReviewsResolver } from './all-reviews/all-reviews.resolver';
 
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AllReviewsComponent,
+    title: 'All Reviews',
+    resolve: { sets: allReviewsResolver },
+  },
   {
     path: ':id/create',
     component: CreateReviewComponent,
