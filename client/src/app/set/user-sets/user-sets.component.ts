e5939e32651b3faca112e9e2a09c8f3825381690
Denis Vasilev <vassdeniss@gmail.com>
2023-09-07 01:46:46 +0300

    Client: Translated user sets

--- client/src/app/set/user-sets/user-sets.component.ts
+++ client/src/app/set/user-sets/user-sets.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { PopupService } from 'src/app/services/popup.service';
 import { Set } from 'src/app/types/setType';
 import { User } from 'src/app/types/userType';
 
@@ -13,15 +12,12 @@ export class UserSetsComponent {
   user: User | undefined = undefined;
   sets: Set[] = [];
 
-  constructor(private route: ActivatedRoute, public popup: PopupService) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.route.data.subscribe(({ data }) => {
       this.sets = data.sets;
       this.user = data.user;
-      if (this.sets.length <= 0) {
-        this.popup.show();
-      }
     });
   }
 }
