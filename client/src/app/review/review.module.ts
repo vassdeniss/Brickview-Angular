cf901a5009c2ae202d6a7941cd39417b76a7eb51
Denis Vasilev <vassdeniss@gmail.com>
2023-11-19 19:19:56 +0200

    Client: Removed edit review component

--- client/src/app/review/review.module.ts
+++ client/src/app/review/review.module.ts
@@ -6,7 +6,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ReviewRoutingModule } from './review-routing.module';
 import { DetailReviewComponent } from './detail-review/detail-review.component';
 import { NgxEditorModule } from 'ngx-editor';
-import { EditReviewComponent } from './edit-review/edit-review.component';
 import { AllReviewsComponent } from './all-reviews/all-reviews.component';
 import { CoreModule } from '../core/core.module';
 import { TranslateModule } from '@ngx-translate/core';
@@ -15,7 +14,6 @@ import { TranslateModule } from '@ngx-translate/core';
   declarations: [
     CreateReviewComponent,
     DetailReviewComponent,
-    EditReviewComponent,
     AllReviewsComponent,
   ],
   imports: [
