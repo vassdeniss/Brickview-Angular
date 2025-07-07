344744cc85c955e9f9d6e022df5ca77075edb76a
Denis Vasilev <vassdeniss@gmail.com>
2025-07-07 21:19:30 +0300

    cors update

--- server/configs/middlewares.js
+++ server/configs/middlewares.js
@@ -2,7 +2,13 @@ const express = require('express');
 const cors = require('cors');
 
 function addMiddlewares(app) {
-  app.use(cors());
+  app.use(
+    cors({
+      origin: 'https://brickview.vasspass.net',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    })
+  );
   app.use(express.json({ limit: '500mb' }));
   app.use(express.urlencoded({ extended: false, limit: '500mb' }));
 }
